0d5fde29ede1c4aedf3b85272e5e8f3f6bd058f6
Nishant Pai <nishantpai@Nishants-MacBook-Pro.local>
2024-04-19 12:59:53 +0530

    add mouse scroll

--- pages/infinitecanvas.js
+++ pages/infinitecanvas.js
@@ -2,7 +2,7 @@
 import Head from 'next/head'
 import { useState, useEffect } from 'react'
 import dynamic from 'next/dynamic'
-import { FiX, FiPlus } from 'react-icons/fi'
+import { FiX, FiPlus, FiHome } from 'react-icons/fi'
 
 class Window {
   constructor(url, width, height, x, y, z = 1) {
@@ -17,6 +17,9 @@ class Window {
 
 class Canvas {
   constructor() {
+    this.cameraX = globalThis.window.innerWidth / 2
+    this.cameraY = globalThis.window.innerHeight / 2
+    this.zoom = 1
     this.windows = []
   }
   // addWindow(window) {
@@ -44,6 +47,7 @@ function InfiniteCanvas() {
   const [height, setHeight] = useState(300)
   const [x, setX] = useState(200)
   const [y, setY] = useState(200)
+
   const [canvas, setCanvas] = useState(new Canvas())
   const [currentWindow, setCurrentWindow] = useState(null)
   const [currentTop, setCurrentTop] = useState(1)
@@ -52,7 +56,23 @@ function InfiniteCanvas() {
 
   const wheelListener = (e) => {
     // console.log('wheel listener')
-    console.log(e.deltaY)
+    let friction = 1;
+    let deltaX = e.deltaX * friction
+    let deltaY = e.deltaY * friction
+    if (e.ctrlKey) {
+      // zoom in and out
+      canvas.zoom += (deltaY / 1000) * -1
+      setCanvas({ ...canvas })
+      globalThis.window.localStorage.setItem('canvas', JSON.stringify(canvas))
+    } else {
+      // scroll set the camera position
+      canvas.cameraX += (deltaX * 2)
+      canvas.cameraY += (deltaY * 2)
+      setCanvas({ ...canvas })
+      globalThis.window.localStorage.setItem('canvas', JSON.stringify(canvas))
+    }
+    console.log(deltaX, deltaY)
+
   }
 
   const pointerMoveListener = (e) => {
@@ -67,14 +87,20 @@ function InfiniteCanvas() {
     }
     if (document.body.style.cursor === 'move' && currentWindow !== null && e.buttons === 1) {
       console.log('current window', canvas.windows[currentWindow].z, canvas.windows[currentWindow].x, canvas.windows[currentWindow].y, canvas.windows[currentWindow].width, canvas.windows[currentWindow].height)
-      canvas.windows[currentWindow].x += e.movementX
-      canvas.windows[currentWindow].y += e.movementY
+      canvas.windows[currentWindow].x += e.movementX * 4
+      canvas.windows[currentWindow].y += e.movementY * 4
       canvas.windows[currentWindow].z = currentTop
       setCurrentTop(currentTop + 1)
       setCanvas({ ...canvas })
       // save canvas to local storage
       window.localStorage.setItem('canvas', JSON.stringify(canvas))
       console.log('pointer move listener', document.body.style.cursor, currentWindow, e.movementX, e.movementY, canvas.windows[currentWindow].x, canvas.windows[currentWindow].y)
+    } 
+    if(e.buttons === 1 && document.body.style.cursor === 'default') {
+      canvas.cameraX += e.movementX * 2
+      canvas.cameraY += e.movementY * 2
+      setCanvas({ ...canvas })
+      globalThis.window.localStorage.setItem('canvas', JSON.stringify(canvas))
     }
   }
 
@@ -104,6 +130,16 @@ function InfiniteCanvas() {
     document.body.style.overflow = 'hidden'
     document.body.style.position = 'fixed'
   }, [])
+
+  const calculateX = (x) => {
+    // compute the x position based on the camera position
+    return x - canvas.cameraX + window.innerWidth / 2
+  }
+
+  const calculateY = (y) => {
+    // compute the y position based on the camera position
+    return y - canvas.cameraY + window.innerHeight / 2
+  }
 
 
   return (
@@ -137,6 +173,7 @@ function InfiniteCanvas() {
           transformOrigin: 'left top',
           width: '100vw',
           height: '100vh',
+          transform: `scale(${canvas.zoom})`,
         }}>
 
           {canvas.windows.map((window, idx) => {
@@ -144,8 +181,8 @@ function InfiniteCanvas() {
               <div style={{
                 width: `${window.width}px`,
                 height: `${window.height}px`,
-                left: `${window.x}px`,
-                top: `${window.y}px`,
+                left: `${calculateX(window.x)}px`,
+                top: `${calculateY(window.y)}px`,
                 zIndex: `${window.z}`,
                 position: 'absolute',
                 border: '3px solid #333',
@@ -202,10 +239,10 @@ function InfiniteCanvas() {
                       console.log('clicked close')
                       canvas.windows.splice(idx, 1)
                       setCanvas({ ...canvas })
-                      if(globalThis.window.localStorage.getItem('canvas')) {
+                      if (globalThis.window.localStorage.getItem('canvas')) {
                         globalThis.window.localStorage.setItem('canvas', JSON.stringify(canvas))
                       }
-                    }}/>
+                    }} />
                   </button>
                   {/* window nav bar */}
                 </div>
@@ -232,13 +269,14 @@ function InfiniteCanvas() {
           })}
 
 
+        </div>
         {/* at the bottom the a url window to add links */}
         <div style={{
           position: 'absolute',
           bottom: 20,
           width: '100%',
           height: '50px',
-          background: '#000',
+          background: 'transparent',
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
@@ -252,28 +290,54 @@ function InfiniteCanvas() {
             border: 'none',
             outline: 'none',
             padding: '19px',
-          }} placeholder="Enter URL" onChange={(e) => { setNewURL(e.target.value)}}/>
+            background: '#222',
+            border: '1px solid #333',
+          }} placeholder="Enter URL" onChange={(e) => { setNewURL(e.target.value) }} />
           <button style={{
             borderRadius: '2px 6px',
-            background: '#333',
+            background: '#222',
             color: '#fff',
             marginLeft: '10px',
             border: 'none',
             outline: 'none',
-            padding: '8px 10px'
+            padding: '8px 10px',
+            border: '1px solid #333',
           }} onClick={() => {
             console.log('clicked add window')
-            canvas.windows.push(new Window(newURL, 300, 300, window.innerWidth / 2 - 150, window.innerHeight / 2 - 150, currentTop + 1))
+            // open new window at the center of the screen
+            canvas.windows.push(new Window(newURL, 300, 300, canvas.cameraX - 150, canvas.cameraY - 150, currentTop + 1))
             setCurrentTop(currentTop + 1)
             setCanvas({ ...canvas })
             setNewURL('')
             globalThis.window.localStorage.setItem('canvas', JSON.stringify(canvas))
           }}>
-            <FiPlus/>
+            <FiPlus />
+          </button>
+          <button
+            style={{
+              borderRadius: '2px 6px',
+              background: '#222',
+              color: '#fff',
+              marginLeft: '10px',
+              border: 'none',
+              outline: 'none',
+              padding: '8px 10px',
+              border: '1px solid #333'
+            }}
+            onClick={() => {
+              // reset camera back to the center and zoom to 1
+              canvas.cameraX = globalThis.window.innerWidth / 2
+              canvas.cameraY = globalThis.window.innerHeight / 2
+              canvas.zoom = 1
+              setCanvas({ ...canvas })
+              globalThis.window.localStorage.setItem('canvas', JSON.stringify(canvas))
+            }}
+          >
+            <FiHome />
           </button>
         </div>
 
-        </div>
+
       </div>
     </>
   )
