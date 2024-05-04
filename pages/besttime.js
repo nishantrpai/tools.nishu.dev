1b6c324de1c8967a832dc437d3d035549858026a
Nishant Pai <nishantpai@Nishants-MacBook-Pro.local>
2024-05-04 18:22:43 +0530

    divide into two sections create and vote

--- pages/besttime.js
+++ pages/besttime.js
@@ -100,6 +100,12 @@ export default function PollTime() {
     13: 1,
   }
 
+  return (
+    <Create />
+  )
+}
+
+function Vote() {
 
   return (
     <>
@@ -107,7 +113,7 @@ export default function PollTime() {
         <div style={{ display: 'flex', flexDirection: 'column' }}>
           {/* creator tz will be from normal mode like 12am to 12pm and my tz will be offset difference to creator */}
           {/* have to start from   */}
-         
+
           <div className={styles.timeCtr}>
             {Array.from({ length: 24 }).map((_, index) => (
               <div>
@@ -119,22 +125,22 @@ export default function PollTime() {
                 }}
                   disabled={(Math.round(creatorHour + index) % 24) >= creatorComfortStart && (Math.round(creatorHour + index) % 24) <= creatorComfortEnd}
                   key={index} >
-                    {/* highest vote */}
-                    <span style={{
-                      // draw on top right of the div
-                      position: 'relative',
-                      top: 0,
-                      left: 10,
-                      padding: '5px',
-                      fontSize: '1.5rem',
-                      transform: 'translate(20%, -60%)',
-                      borderRadius: '5px',
-                      color: '#ca8a04',
-                      // show only highest vote
-                      visibility: Object.values(votesArray).includes(Math.max(...Object.values(votesArray))) && votesArray[Math.round(creatorHour + index) % 24] == Math.max(...Object.values(votesArray)) ? 'visible' : 'hidden',
-                    }}>
-                      🥇
-                    </span>
+                  {/* highest vote */}
+                  <span style={{
+                    // draw on top right of the div
+                    position: 'relative',
+                    top: 0,
+                    left: 10,
+                    padding: '5px',
+                    fontSize: '1.5rem',
+                    transform: 'translate(20%, -60%)',
+                    borderRadius: '5px',
+                    color: '#ca8a04',
+                    // show only highest vote
+                    visibility: Object.values(votesArray).includes(Math.max(...Object.values(votesArray))) && votesArray[Math.round(creatorHour + index) % 24] == Math.max(...Object.values(votesArray)) ? 'visible' : 'hidden',
+                  }}>
+                    🥇
+                  </span>
                   <span className={styles.hour}>
                     {Math.round(creatorHour + index) % 24 < 10 ? '0' + Math.round(creatorHour + index) % 24 : Math.round(creatorHour + index) % 24}
                   </span>
@@ -166,6 +172,78 @@ export default function PollTime() {
         </div>
       </main>
     </>
-  )
 
+  )
+}
+
+function Create() {
+  // create poll
+  const [roomGoal, setRoomGoal] = useState('');
+  const [startBestTime, setStartBestTime] = useState('');
+  const [endBestTime, setEndBestTime] = useState('');
+
+  const createRoom = () => {
+    const socket = io();
+    socket.emit("createRoom", {
+      goal: roomGoal,
+      bestStartTime: startBestTime,
+      bestEndTime: endBestTime,
+      tz: new Date().getTimezoneOffset() / 60 * -1,
+    });
+    socket.on("roomCreated", (roomId) => {
+      setRoomId(roomId);
+      router.push(`/besttime?poll=${roomId}`);
+    });
+  }
+
+  return (
+    <>
+      <main>
+        <h1>Create a poll</h1>
+        <p style={{ color: "#888" }} >Find out what is the best time for you and friends to connect online</p>
+        <div style={{ display: 'flex', flexDirection: 'column', width: '100%', gap: 10, alignContent: 'flex-start', border: '1px solid #333', padding: '10px', borderRadius: 5 }}>
+
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <span style={{ color: '#999', fontSize: 12, marginBottom: 14 }}>Question</span>
+
+            <input
+              style={{ border: 'none', background: '#111', border: '1px solid #333', padding: '5px 10px', borderRadius: '5px' }}
+              type="text" value={roomGoal} onChange={(e) => setRoomGoal(e.target.value)}
+              placeholder='Enter a question like "What is the best time to mint this collection together?"'
+            />
+          </div>
+          <p style={{ color: '#888', fontSize: 14, marginTop: 14, marginBottom: 14 }}>
+            In your timezone, what is the best time to start and end the event?
+          </p>
+          <div style={{ display: 'flex', width: '100%', gap: 10 }}>
+            <div style={{ display: 'flex', flexDirection: 'column', width: '50%', gap: 10 }}>
+              <span 
+              style={{ color: '#999', fontSize: 12 }}>From:</span>
+              <input
+                style={{ border: 'none', background: '#111', border: '1px solid #333', padding: '5px 10px', borderRadius: '5px' }}
+                type="number"
+                value={startBestTime}
+                onChange={(e) => setStartBestTime(e.target.value)}
+                placeholder='Enter a number between 0 and 24'
+              />
+            </div>
+
+            <div style={{ display: 'flex', flexDirection: 'column', width: '50%', gap: 10 }}>
+              <span style={{ color: '#999', fontSize: 12 }}>To:</span>
+              <input
+                style={{ border: 'none', background: '#111', border: '1px solid #333', padding: '5px 10px', borderRadius: 5 }}
+                type="number"
+                value={endBestTime}
+                onChange={(e) => setEndBestTime(e.target.value)}
+                placeholder='Enter a number between 0 and 24'
+              />
+            </div>
+
+          </div>
+          <button style={{ margin: 'auto', marginTop: '20px' }} onClick={() => createRoom()}>Start Poll</button>
+        </div>
+        <p>Preview:</p>
+      </main>
+    </>
+  )
 }
