ef219c37dc849a67dd8f647f4c510df7ef051a3a
Nishant Pai <nishantpai@Nishants-MacBook-Pro.local>
2024-05-11 22:32:07 +0530

    get member data

--- pages/lovecaster.js
+++ pages/lovecaster.js
@@ -32,19 +32,104 @@ export default function LoveCaster() {
 
   const [page, setPage] = useState('profiles');
 
+  const formatSummary = (summary) => {
+    // summary is a json object with keys about, like, dislike
+    if (summary === 'Loading...') return summary
+    let formattedSummary = JSON.parse(summary)
+    let sections = []
+    for (let key in formattedSummary) {
+      let elem =
+        <div key={key} style={{ display: 'flex', flexDirection: 'column', gap: '10px', marginBottom: '10px' }}>
+          <h4>{key.toUpperCase()}</h4>
+          <ul style={{ listStyle: 'none' }}>
+            {formattedSummary[key].map((item, index) => <li style={{ color: '#ccc', marginBottom: '5px' }} key={index}>- {item.toLowerCase()}</li>)}
+          </ul>
+        </div>
+      sections.push(elem)
+    }
+    return sections
+  }
+
+  const summarize = (casts, username) => {
+    return new Promise((resolve, reject) => {
+      let prompt = `Given the tweets of ${username}: ${casts.join(' ')}, summarize what ${username} does and what they like/dislike. 
+    Tips for summarizing: 0. respond in json and the keys should be lower case 
+    1. Keep 3 sections of concise information, first is "about", "like" and third is "dislike". 
+    2. Don't summarize what is obvious, read between the lines and see the repeating patterns. 
+    3. Avoid recency bias. 
+    4. Replace any mention of twitter with farcaster or tweets with posts.  
+    5. No quotes. 
+    6. Speak in third person.
+    7. Don't repeat the name, just share the information.
+    8. Each point will be value in array.`
+      fetch(`/api/gpt?prompt`, {
+        method: 'POST',
+        body: JSON.stringify({ prompt }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(res => res.json())
+        .then(data => {
+          resolve(data.response);
+        })
+    });
+  }
+
+
+  const fetchTweets = (username) => {
+    return new Promise((resolve, reject) => {
+      fetch(`https://searchcaster.xyz/api/profiles?username=${username}`)
+        .then(res => res.json())
+        .then(data => {
+          let fid = data[0].body.id
+          let profile = data[0].body
+          let url = encodeURIComponent(`https://client.warpcast.com/v2/casts?fid=${fid}&limit=30`)
+
+          fetch(`https://cors-proxy-production-a6e6.up.railway.app/?url=${url}`).then(res => res.json()).then(data => {
+            let casts = data.result.casts
+            casts = casts.filter(cast => cast.author.fid === fid)
+            casts = casts.map(cast => cast.text)
+            casts = casts.filter(cast => cast.length > 0)
+            resolve({casts, profile})
+          })
+        })
+    });
+  }
+
+
+  const fetchMemberData = async (username) => {
+    return new Promise((resolve, reject) => {
+      fetchTweets(username).then(async ({casts, profile}) => {
+        console.log('casts', casts.length);
+        let summary = await summarize(casts, username);
+        resolve({
+          name:  profile.displayName,
+          username: profile.username,
+          img: profile.avatarUrl,
+          bio: profile.bio,
+          summary: summary,
+        });
+      });
+    });
+
+    // return {
+    //   name: 'nishu',
+    //   username: 'nish',
+    //   img: 'https://i.seadn.io/gae/SypPjsAZsAaiNvsh2V7w8M1PXz7o2t0EFNb4Jd04yx5y_rtr7MeA4fFPRlSK-3M9b5Vv7EoF8W7BVHEKZ_NhqQrsBhtTi9hieFk8CXg?w=500&auto=format&',
+    //   bio: 'I am a software engineer',
+    //   summary: 'Loading...',
+    // }
+  }
+
   useEffect(() => {
     // for each member we have to add bio, username image and about if it doesn't exist
     accounts.forEach(account => {
       if (!members.find(member => member.username === account.username)) {
         console.log('adding member', account.username, members);
-        setMembers(prev => [...prev, {
-          name: account.username,
-          username: account.username,
-          img: 'https://i.seadn.io/gae/SypPjsAZsAaiNvsh2V7w8M1PXz7o2t0EFNb4Jd04yx5y_rtr7MeA4fFPRlSK-3M9b5Vv7EoF8W7BVHEKZ_NhqQrsBhtTi9hieFk8CXg?w=500&auto=format&',
-          bio: 'I am a warp caster',
-          about: 'I am a warp caster',
-        }]
-        );
+        fetchMemberData(account.username).then(data => {
+          setMembers(prev => [...prev, data]);
+        });
       }
     });
   }, [accounts]);
@@ -63,14 +148,13 @@ export default function LoveCaster() {
   const Profiles = () => {
     return (
       <>
-        {members.length}
         {/* radio button of male or female */}
-        <select value={preferences} onChange={(e) => {
+        {/* <select value={preferences} onChange={(e) => {
           setPreferences(e.target.value);
-        }}> 
+        }}>
           <option value={'male'}>male</option>
           <option value={'female'}>female</option>
-        </select>
+        </select> */}
         <div style={{
           display: 'flex',
           width: 'max-content',
@@ -80,7 +164,7 @@ export default function LoveCaster() {
           paddingTop: 100,
           zoom: 1.25,
         }}>
-          {members.map((member, index) => (
+          {members.length > 0 ? members.map((member, index) => (
             <div className={styles.tinderCard} id={`tinder-card-${index}`} style={{ opacity: 1 - index * 0.1, zIndex: 10 - index, top: 70 + ((index % 5) * 5), borderColor: `rgba(51,51,51,${(index + 1) * 0.95})` }}>
               {/* about */}
               <div>
@@ -92,7 +176,7 @@ export default function LoveCaster() {
                     <p className={styles.bio}>{member.bio}</p>
                   </div>
                 </div>
-                <p className={styles.about}>{member.about}</p>
+                <p className={styles.about}>{formatSummary(member.summary)}</p>
               </div>
               <div className={styles.swipeMenu}>
                 <button className={styles.swipeButton} style={{ color: '#f87171' }} onClick={() => {
@@ -124,7 +208,7 @@ export default function LoveCaster() {
               </div>
             </div>
 
-          ))}
+          )) : null}
 
         </div>
         {/* at the bottom of main */}
