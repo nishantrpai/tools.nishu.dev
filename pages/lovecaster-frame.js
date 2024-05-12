d454b1b4020fcbdc3b2b17062864729dbde2902a
Nishant Pai <nishantpai@Nishants-MacBook-Pro.local>
2024-05-12 06:53:55 +0530

    lovecaster frame for now, won't be using it

--- pages/lovecaster-frame.js
+++ pages/lovecaster-frame.js
@@ -8,8 +8,9 @@ export default function Page() {
       <meta property="fc:frame" content="vNext" />
       <meta property="fc:frame:image" content="https://supabase.com/images/blog/launch-week-7/designing-with-ai-midjourney/header.png"/>
       <meta property="og:image" content="https://supabase.com/images/blog/launch-week-7/designing-with-ai-midjourney/header.png"/>
-      <meta name="fc:frame:post_url" content="https://frame.example.com/start" />
-      <meta name="fc:frame:button:1" content="Start" />
+
+      <meta name="fc:frame:post_url" content="https://fc-polls.vercel.app/api/vote?id=054aee65-c63d-46c1-a1f9-a05b747860f6" />
+<meta name="fc:frame:button:1" content="Start" />
     </Head>
     </>
   )
