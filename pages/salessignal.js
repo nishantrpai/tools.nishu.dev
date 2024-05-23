cc2c5a821ea59d598523beb783270c91c0563fe3
Nishant Pai <nishantpai@Nishants-MacBook-Pro.local>
2024-05-23 06:45:06 +0530

    track sales signals

---
+++ pages/salessignal.js
@@ -0,0 +1,63 @@
+// sales signal to track to get new users
+import Head from 'next/head'
+import styles from '@/styles/Home.module.css'
+import { useState, useEffect } from 'react'
+
+
+export default function Home() {
+  const [text, setText] = useState('')
+  const [sensationalizedText, setSensationalizedText] = useState('')
+  const [loading, setLoading] = useState(false)
+  
+  const sensationalize = async () => {
+    // make api call to /api/gpt?prompt
+    setLoading(true)
+    const res = await fetch(`/api/gpt?prompt='"Given an app idea: ${text}", what sales signal should I track to know if it's a good idea? For e.g., if it is a privacy app, how many companies are selling user data and such related signals.'`)
+    const data = await res.json()
+    setSensationalizedText(data.response)
+    setLoading(false)
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Sales Signal</title>
+        <meta name="description" content="What sales signal should I track to know if there is market" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.container}>
+        <h1 className={styles.title}>
+          Sales Signal
+        </h1>
+        <span style={{ color: '#777', fontSize: '14px', marginBottom: '20px', display: 'block' }}>
+          What sales signal should I track to know if there is market
+        </span>
+
+
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="Enter your text"
+          style={{
+            width: '100%',
+            border: '1px solid #333',
+            padding: '10px',
+            outline: 'none',
+          }}
+        />
+
+        <button onClick={sensationalize} className={styles.button}>
+          {loading ? 'Analyzing...' : 'Analyze'}
+        </button>
+
+        {sensationalizedText && (
+          <div style={{ whiteSpace: 'pre-wrap', fontFamily: 'monospace', textAlign: 'left', padding: '10px', border: '1px solid #333', background: '#000', borderRadius: 10, width: '100%', lineHeight: 1.5}}>
+            {sensationalizedText}
+          </div>
+        )}
+      </main>
+    </>
+  )
+}
