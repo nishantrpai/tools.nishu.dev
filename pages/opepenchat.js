d5525244721ca74326398645fb09868450bd102c
Nishant Pai <nishantpai@Nishants-MacBook-Pro.local>
2024-07-05 20:35:57 +0530

    fix download issues

--- pages/opepenchat.js
+++ pages/opepenchat.js
@@ -1,14 +1,11 @@
-// there'll be a canvas with an image drawn on it right side
-//  image will be taken from contract
-// will have a little fade on the image at the bottom
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { ethers } from 'ethers'
 import html2canvas from 'html2canvas'
 
 export default function OpepenChat() {
-  let RPC_CHAINS = {
+  const RPC_CHAINS = {
     'ETHEREUM': {
       'rpc': 'https://eth-pokt.nodies.app',
       'chainId': 1,
@@ -25,15 +22,13 @@ export default function OpepenChat() {
   const [token1, setToken1] = useState('1')
   const [opepen, setOpepen] = useState('')
   const [title, setTitle] = useState('')
+  const canvasRef = useRef(null)
 
   const getNFTData = async (collection_address, id) => {
-    // get metadata of nft and ipfs hash
     try {
       const provider = new ethers.JsonRpcProvider(RPC_CHAINS[chain].rpc)
       const contract = new ethers.Contract(collection_address, ['function tokenURI(uint256) view returns (string)'], provider)
       const tokenURI = await contract.tokenURI(id)
-      // if tokenURI is a url, fetch the metadata, if it is ipfs replace with ipfs.io
-      // if token uri is data:// json parse it
       if (tokenURI.startsWith('data:')) {
         const metadata = JSON.parse(atob(tokenURI.split('data:application/json;base64,')[1]))
         if (metadata.image.startsWith('ipfs://')) {
@@ -47,7 +42,6 @@ export default function OpepenChat() {
         if (metadata.image.startsWith('ipfs://')) {
           metadata.image = `https://ipfs.io/ipfs/${metadata.image.split('ipfs://')[1]}`
         }
-
         return metadata
       } else {
         const ipfsHash = tokenURI.split('ipfs://')[1]
@@ -57,7 +51,6 @@ export default function OpepenChat() {
         if (metadata.image.startsWith('ipfs://')) {
           metadata.image = `https://ipfs.io/ipfs/${metadata.image.split('ipfs://')[1]}`
         }
-
         return metadata
       }
     } catch (e) {
@@ -66,29 +59,44 @@ export default function OpepenChat() {
   }
 
   useEffect(() => {
-    // draw image on canvas
-    const canvas = document.getElementById('canvas')
-    const context = canvas.getContext('2d')
-    context.beginPath()
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+    context.beginPath();
     if (opepen) {
-      const image = new Image()
-      image.src = opepen
+      const image = new Image();
+      image.crossOrigin = 'anonymous'
+      image.src = opepen;
       image.onload = () => {
-        context.clearRect(0, 0, canvas.width, canvas.height)
-        // resize image to fit canvas and draw 20px away from right
-        const scaleX = canvas.width / image.width
-        const scaleY = canvas.height / image.height
-        const scale = Math.max(scaleX, scaleY)
-        const x = (canvas.width / 2) - (image.width / 2) * scale
-        const y = 0
-        context.drawImage(image, x, y, image.width * scale, image.height * scale)
-        context.fillStyle = 'rgba(0, 0, 0, 0.5)'
-        context.fillRect(0, 0, canvas.width, canvas.height)
-        context.closePath()
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        const scaleX = canvas.width / image.width;
+        const scaleY = canvas.height / image.height;
+        const scale = Math.max(scaleX, scaleY);
+        const x = (canvas.width / 2) - (image.width / 2) * scale;
+        const y = 0;
+        context.drawImage(image, x, y, image.width * scale, image.height * scale);
+        context.fillStyle = 'rgba(0, 0, 0, 0.5)';
+        context.fillRect(0, 0, canvas.width, canvas.height);
+        context.closePath();
       }
     }
-  }, [opepen])
+  }, [opepen]);
 
+  const downloadImage = () => {
+    const opepenCover = document.getElementById('opepen-cover');
+    html2canvas(opepenCover, {
+      allowTaint: true,
+      backgroundColor: null,
+      useCORS: true,
+    }).then(canvas => {
+      let img = canvas.toDataURL('image/png');
+      let a = document.createElement('a');
+      a.href = img;
+      a.download = 'profile.png';
+      a.click();
+    }).catch(err => {
+      console.error('Error capturing div:', err);
+    });
+  }
 
   return (
     <>
@@ -111,27 +119,27 @@ export default function OpepenChat() {
         </span>
 
         <div style={{
-          width: 'max-content',
+          width: '1024px',
+          height: '700px',
           position: 'relative',
+          backgroundColor: '#000',
         }} id="opepen-cover">
-          {/* title on top of canvas on top left */}
-
-          <div style={{
+          <span style={{
             position: 'absolute',
             top: 0,
             left: 0,
-            color: '#aaa',
+            color: '#fff',
+            opacity: 0.75,
             padding: '10px',
             fontFamily: 'opepen',
             fontSize: '2em',
             whiteSpace: 'pre',
           }}>
             Opepen Chat
-          </div>
-
-          <div style={{
+          </span>
+          <span style={{
             position: 'absolute',
-            top: 40,
+            top: 60,
             left: 0,
             color: '#fff',
             padding: '10px',
@@ -140,42 +148,46 @@ export default function OpepenChat() {
             whiteSpace: 'pre',
           }}>
             {title}
-          </div>
-          <canvas id="canvas" width={1024} height={700} style={{ border: '1px solid #333' }}>
-
-          </canvas>
-
+          </span>
+          <canvas ref={canvasRef} id="canvas" width={1024} height={700} style={{ border: '1px solid #333' }}></canvas>
         </div>
 
-        <div style={{ display: 'flex', flexDirection: 'column', gap: 20, justifyContent: 'center', marginTop: '20px' }}>
-          {/* <input type="text" placeholder="Collection Address" value={collectionAddress} onChange={(e) => setCollectionAddress(e.target.value)} /> */}
-          <textarea placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
-          <input type="text" placeholder="Token ID" value={token1} onChange={(e) => setToken1(e.target.value)} />
+        <div style={{ display: 'flex', flexDirection: 'column', gap: 20, justifyContent: 'center', marginTop: '20px', width: '100%' }}>
+          <textarea placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)}  style={{
+            backgroundColor: '#000',
+            color: '#fff',
+            border: '1px solid #333',
+            borderRadius: 10,
+            padding: '10px',
+            font: '16px',
+            width:'100%', height: '200px'
+          }}></textarea>
+          <input type="text" 
+          placeholder="Token ID" value={token1} onChange={(e) => setToken1(e.target.value)} style={{
+            backgroundColor: '#000',
+            color: '#fff',
+            border: '1px solid #333',
+            borderRadius: 10,
+            padding: '10px',
+            font: '16px',
+            width:'100%'
+          }}/>
           <button onClick={() => {
             getNFTData(collectionAddress, token1).then((data) => {
-              if (!data) return
-              setOpepen(data.image)
-            })
-          }}>Get Opepen</button>
-          <button onClick={() => {
-            // copy tier-list to clipboard as image
-            const opepenCover = document.getElementById('opepen-cover')
-            html2canvas(opepenCover, {
-              allowTaint: true,
-              backgroundColor: 'transparent',
-              useCORS: true,
-            }).then(canvas => {
-              let img = canvas.toDataURL('image/png')
-              let a = document.createElement('a')
-              a.href = img
-              a.download = 'profile.png'
-              a.click()
+              if (!data) return;
+              setOpepen(data.image);
             });
+          }} style={{
+            width: '100%',
+            padding: '10px',
+          }}>Change Background</button>
+          <button onClick={downloadImage} style={{
+            width: '100%',
+            padding: '10px',
           }}>
             Download
           </button>
         </div>
-
       </main>
     </>
   )
