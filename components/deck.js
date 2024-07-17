39290b21703e1ae4e40549385e5fe9c77e9ce0a4
Nishant Pai <nishantpai@Nishants-MacBook-Pro.local>
2024-07-17 12:23:40 +0530

    update 300px

--- components/deck.js
+++ components/deck.js
@@ -53,7 +53,7 @@ const Deck = () => {
     <div style={{
       height: 'max-content',
       minHeight: '100vh',
-      width: '200px',
+      width: '300px',
       border: '1px solid #333',
       padding: '20px',
     }}>
