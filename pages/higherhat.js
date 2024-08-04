f065c64bea71ead401d1be268d23f36b9b33acfc
Nishant Pai <nishantpai@Nishants-MacBook-Pro.local>
2024-08-04 08:17:10 +0530

    add higher crown

--- pages/higherhat.js
+++ pages/higherhat.js
@@ -16,6 +16,7 @@ export default function HigherHat() {
   const higherHat = '/higherhat.png'
   const higherHat2 = '/higherhat2.png'
   const higherHat3 = '/higherhat3.png'
+  const higherCrown = '/highercrown.png'
 
   useEffect(() => {
     // draw image on canvas
@@ -34,6 +35,8 @@ export default function HigherHat() {
         hat.src = higherHat2
       else if (hatType === 2)
         hat.src = higherHat3
+      else if (hatType === 3)
+        hat.src = higherCrown
 
       hat.onload = () => {
         context.translate(offsetX, offsetY)
@@ -90,8 +93,9 @@ export default function HigherHat() {
           height: 'auto',
           margin: '20px 0'
         }}></canvas>
-        <div style={{ display: 'flex', gap: 20, flexDirection: 'column', width: '50%' }}>
-          <div style={{ display: 'flex', gap: 20, justifyContent: 'space-between', margin: 'auto' }}>
+        
+        <div style={{ display: 'flex', gap: 20, flexDirection: 'column', width: '100%', justifyContent: 'center', alignItems: 'center' }}>
+          <div style={{ display: 'flex', gap: 20, justifyContent: 'space-between' }}>
             <div style={{
               width: 100,
               height: 100,
@@ -105,31 +109,43 @@ export default function HigherHat() {
                 marginTop: 20,
               }} onClick={() => setHatType(0)} />
             </div>
-            <div style={{
-              width: 100,
-              height: 100,
-              border: hatType === 1 ? '2px solid #333' : '2px solid #111',
-              borderRadius: 10
-            }}>
-              <img src={higherHat2} alt="Higher Hat 2" style={{
-                width: '80%', height: 'auto',
-                margin: 'auto',
-                marginTop: 30,
-                marginLeft: 10
-              }} onClick={() => setHatType(1)} />
-            </div>
-            <div style={{
-              width: 100,
-              height: 100,
-              border: hatType === 2 ? '2px solid #333' : '2px solid #111',
-              borderRadius: 10
-            }}>
-              <img src={higherHat3} alt="Higher Hat 3" style={{
-                width: 100, height: 'auto',
-                marginTop: 20,
-              }} onClick={() => setHatType(2)} />
-            </div>
           </div>
+          <div style={{
+            width: 100,
+            height: 100,
+            border: hatType === 1 ? '2px solid #333' : '2px solid #111',
+            borderRadius: 10
+          }}>
+            <img src={higherHat2} alt="Higher Hat 2" style={{
+              width: '80%', height: 'auto',
+              margin: 'auto',
+              marginTop: 30,
+              marginLeft: 10
+            }} onClick={() => setHatType(1)} />
+          </div>
+          <div style={{
+            width: 100,
+            height: 100,
+            border: hatType === 2 ? '2px solid #333' : '2px solid #111',
+            borderRadius: 10
+          }}>
+            <img src={higherHat3} alt="Higher Hat 3" style={{
+              width: 100, height: 'auto',
+              marginTop: 20,
+            }} onClick={() => setHatType(2)} />
+          </div>
+        <div style={{
+          width: 100,
+          height: 100,
+          border: hatType === 3 ? '2px solid #333' : '2px solid #111',
+          borderRadius: 10
+        }}>
+          <img src={higherCrown} alt="Higher Crown" style={{
+            width: 100, height: 'auto',
+            marginTop: 20,
+          }} onClick={() => setHatType(3)} />
+                  </div>
+
           <label>
             Offset X
           </label>
@@ -161,7 +177,7 @@ export default function HigherHat() {
           Download Image
         </button>
         <div style={{ textAlign: 'center', marginTop: 20, color: '#777', fontSize: 12 }}>
-          Higher hat by  <a href="https://warpcast.com/clfx.eth" style={{ color: '#fff', textDecoration: 'underline'}} target='_blank'>clfx</a>, smiley hat by <a href="https://warpcast.com/chicbangs.eth" style={{ color: '#fff', textDecoration: 'underline'}} target='_blank'>Chic</a>, pixel hat by <a href="https://warpcast.com/catra.eth" style={{ color: '#fff', textDecoration: 'underline'}} target='_blank'>catra</a>
+          Higher hat by  <a href="https://warpcast.com/clfx.eth" style={{ color: '#fff', textDecoration: 'underline' }} target='_blank'>clfx</a>, smiley hat by <a href="https://warpcast.com/chicbangs.eth" style={{ color: '#fff', textDecoration: 'underline' }} target='_blank'>Chic</a>, pixel hat by <a href="https://warpcast.com/catra.eth" style={{ color: '#fff', textDecoration: 'underline' }} target='_blank'>catra</a>
         </div>
       </main>
     </>
