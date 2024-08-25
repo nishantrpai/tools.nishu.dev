8e0f5b465d6e1bd4db60393e59d7b5dfa4d863bb
Nishant Pai <nishantpai@Nishants-MacBook-Pro.local>
2024-08-25 21:25:13 +0530

    disable analytics

--- utils/analytics.js
+++ utils/analytics.js
@@ -1,19 +1,20 @@
 // send api request to /analytics with event and metadata
 
 export const analytics = async (event, metadata) => {
-  try {
-    console.log('Sending analytics:', event, metadata)  
-    const res = await fetch('/api/analytics', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ event, metadata })
-    })
-    const data = await res.json()
-    return data
-  } catch (error) {
-    console.error('Error sending analytics:', error)
-    return { success: false, error: 'Failed to send analytics' }
-  }
+  console.log('avoid sending analytics for now')
+  // try {
+  //   console.log('Sending analytics:', event, metadata)  
+  //   const res = await fetch('/api/analytics', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify({ event, metadata })
+  //   })
+  //   const data = await res.json()
+  //   return data
+  // } catch (error) {
+  //   console.error('Error sending analytics:', error)
+  //   return { success: false, error: 'Failed to send analytics' }
+  // }
 }
