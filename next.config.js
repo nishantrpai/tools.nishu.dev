29958b5f19ceb95b3edd17d71d2813df414d61ba
Nishant Pai <nishantpai@Nishants-MacBook-Pro.local>
2024-09-12 12:44:15 +0530

    limit so no one else is able to access apis

--- next.config.js
+++ next.config.js
@@ -3,6 +3,18 @@ const nextConfig = {
   reactStrictMode: false,
   fs: false,
   swcMinify: false,
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: 'https://tools.nishu.dev' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, DELETE, OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
