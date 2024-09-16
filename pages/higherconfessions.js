d98dbc1d7b0bd646f56c4cf476152108c16ba644
Nishant Pai <nishantpai@Nishants-MacBook-Pro.local>
2024-09-16 17:13:33 +0530

    download confession

--- pages/higherconfessions.js
+++ pages/higherconfessions.js
@@ -91,7 +91,7 @@ export default function HigherConfessions() {
             a.download = 'higher-confession.png'
             a.click()
           }
-        }}>Download Image</button>
+        }}>Download Confession</button>
       </main>
     </>
   )
