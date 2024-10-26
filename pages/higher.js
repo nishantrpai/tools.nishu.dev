931228a626838a20f824e1bdea576428cad9e3b3
nishu <nishantrpai@gmail.com>
2024-10-26 20:52:21 +0530

    change title and description

--- pages/higher.js
+++ pages/higher.js
@@ -1490,8 +1490,8 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Tools by Nishu</title>
-        <meta name="description" content="tools.nishu.dev" />
+        <title>Higher Tools</title>
+        <meta name="description" content="Higher tools" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
