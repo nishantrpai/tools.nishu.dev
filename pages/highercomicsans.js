ac39ab70ff652bc35a0d279fe6f961fbda84d5eb
nishantrpai <nishantrpai@gmail.com>
2024-12-17 10:24:23 +0530

    import this font in the styles, might need to define too please make sure to change the format or anything so any element or svg imported can consume

--- pages/highercomicsans.js
+++ pages/highercomicsans.js
@@ -2,12 +2,9 @@
 import Head from 'next/head';
 import styles from '@/styles/Home.module.css';
 import { useState, useEffect } from 'react';
-
-;
+const higherHat = '/higherhelvetica.svg';
 const higherHat2 = '/higherhelvetica2.svg';
 const higherHat3 = '/higherhelvetica3.svg';
-import styles from '@/styles/Home.module.css'
-import { useState, useEffect } from 'react'
 
 export default function HigherHat() {
   const [image, setImage] = useState(null)
@@ -42,95 +39,10 @@ export default function HigherHat() {
       hat.onload = () => {
         context.translate(offsetX, offsetY)
         context.rotate(offsetTheta * Math.PI / 180)
-        context.drawImage(hat, offsetX, offsetY, hat.width * scale, hat.height * scale)
+        context.drawImage(hat, 0, 0, hat.width * scale, hat.height * scale)
               };
     }
   }, [image, offsetX, offsetY, scale, offsetTheta, hatType, imgWidth, imgHeight]);
-
-  return (
-    <>
-      <Head>
-        <title>Higher Comic Sans</title>
-        <meta name="description" content="Higher Comic Sans" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <h1 className={styles.title}>
-          Higher Comic Sans
-        </h1>
-        <span style={{
-          width: '100%',
-          textAlign: 'center',
-          color: '#666',
-          fontSize: '14px'
-        }}>
-          Add higher comicsans on any image
-        </span>
-
-        {/* upload photo */}
-        <input type="file" accept="image/*" onChange={(event) => {
-          const file = event.target.files[0];
-          const reader = new FileReader();
-          reader.onload = () => {
-            const img = new Image();
-            img.src = reader.result;
-            img.onload = () => {
-              setOffsetX(0);
-              setOffsetY(img.height / 2);
-              setScale(2.4);
-              setOffsetTheta(0);
-              setImgWidth(img.width);
-              setImgHeight(img.height);
-              setImage(img);
-            };
-          };
-          reader.readAsDataURL(file);
-        }} />
-        <canvas id="canvas" width="800" height="800" style={{
-          border: '1px solid #333',
-          borderRadius: 10,
-          width: '100%',
-          height: 'auto',
-          margin: '20px 0'
-        }}></canvas>
-        <div style={{ display: 'flex', gap: 20, flexDirection: 'column', width: '50%' }}>
-          <label>
-            Offset X
-          </label>
-          <input type="range" min={-(imgWidth * 1.5)} max={(imgWidth * 1.5)} value={offsetX} onChange={(e) => setOffsetX(parseInt(e.target.value, 10))} /><input type="number" value={offsetX} onChange={(e) => setOffsetX(parseInt(e.target.value, 10))} />
-          <label>
-            Offset Y
-          </label>
-          <input type="range" min={-(imgHeight * 1.5)} max={(imgHeight * 1.5)} value={offsetY} onChange={(e) => setOffsetY(parseInt(e.target.value, 10))} /><input type="number" value={offsetY} onChange={(e) => setOffsetY(parseInt(e.target.value, 10))} />
-          <label>
-            Scale
-          </label>
-          <input type="range" min={0} max={10} step={0.01} value={scale} onChange={(e) => setScale(parseFloat(e.target.value))} /><input type="number" value={scale} onChange={(e) => setScale(parseFloat(e.target.value))} />
-          <label>
-            Rotate
-          </label>
-          <input type="range" min={-360} max={360} value={offsetTheta} onChange={(e) => setOffsetTheta(parseInt(e.target.value, 10))} /><input type="number" value={offsetTheta} onChange={(e) => setOffsetTheta(parseInt(e.target.value, 10))} />
-        </div>
-
-        <button onClick={() => {
-          const canvas = document.getElementById('canvas');
-          const dataURL = canvas.toDataURL('image/png');
-          const a = document.createElement('a');
-          a.href = dataURL;
-          a.download = `highercomicsans-${Date.now()}.png`;
-          a.click();
-        }} style={{
-          marginTop: 20
-        }}>
-          Download Image
-        </button>
-      </main>
-    </>
-  );
-}
-      }
-    }
-  }, [image, offsetX, offsetY, scale, offsetTheta, hatType])
 
   return (
     <>
@@ -161,7 +73,7 @@ export default function HigherHat() {
             img.src = reader.result
             img.onload = () => {
               setOffsetX(0)
-              setOffsetY(104)
+              setOffsetY(img.height / 2)
               setScale(2.4)
               setOffsetTheta(0)
               setImgWidth(img.width)
@@ -182,19 +94,19 @@ export default function HigherHat() {
           <label>
             Offset X
           </label>
-          <input type="range" min={-(imgWidth * 1.5)} max={(imgWidth * 1.5)} value={offsetX} onChange={(e) => setOffsetX(e.target.value)} /><input type="number" value={offsetX} onChange={(e) => setOffsetX(e.target.value)} />
+          <input type="range" min={-(imgWidth * 1.5)} max={(imgWidth * 1.5)} value={offsetX} onChange={(e) => setOffsetX(parseInt(e.target.value, 10))} /><input type="number" value={offsetX} onChange={(e) => setOffsetX(parseInt(e.target.value, 10))} />
           <label>
             Offset Y
           </label>
-          <input type="range" min={-(imgHeight * 1.5)} max={(imgHeight * 1.5)} value={offsetY} onChange={(e) => setOffsetY(e.target.value)} /><input type="number" value={offsetY} onChange={(e) => setOffsetY(e.target.value)} />
+          <input type="range" min={-(imgHeight * 1.5)} max={(imgHeight * 1.5)} value={offsetY} onChange={(e) => setOffsetY(parseInt(e.target.value, 10))} /><input type="number" value={offsetY} onChange={(e) => setOffsetY(parseInt(e.target.value, 10))} />
           <label>
             Scale
           </label>
-          <input type="range" min={0} max={10} step={0.01} value={scale} onChange={(e) => setScale(e.target.value)} /><input type="number" value={scale} onChange={(e) => setScale(e.target.value)} />
+          <input type="range" min={0} max={10} step={0.01} value={scale} onChange={(e) => setScale(parseFloat(e.target.value))} /><input type="number" value={scale} onChange={(e) => setScale(parseFloat(e.target.value))} />
           <label>
             Rotate
           </label>
-          <input type="range" min={-360} max={360} value={offsetTheta} onChange={(e) => setOffsetTheta(e.target.value)} /><input type="number" value={offsetTheta} onChange={(e) => setOffsetTheta(e.target.value)} />
+          <input type="range" min={-360} max={360} value={offsetTheta} onChange={(e) => setOffsetTheta(parseInt(e.target.value, 10))} /><input type="number" value={offsetTheta} onChange={(e) => setOffsetTheta(parseInt(e.target.value, 10))} />
         </div>
 
         <button onClick={() => {
@@ -213,3 +125,5 @@ export default function HigherHat() {
     </>
   )
 }
+      }
+    }
